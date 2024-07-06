d6cb7fff73e211268a0f4225b894fc1af9241539
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-06 12:26:36 -0700

    Update 0-main.py

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """
-0-main
+Queries the Reddit API and prints the number of subscribers for a given subreddit.
 """
-import sys
+import sys  # Importing in alphabetical order
 
 if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        subreddit = sys.argv[1]
-        num_subs = number_of_subscribers(subreddit)
-        print(f"Number of subscribers for r/{subreddit}: {num_subs}")
+        number_of_subscribers = __import__('0-subs').number_of_subscribers
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
