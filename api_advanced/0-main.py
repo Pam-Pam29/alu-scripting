99d865eb45896b9fcdc2fb4c5500f18c33230dee
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-06 13:03:30 -0700

    Update 0-main.py

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -7,14 +7,6 @@ import sys
 if __name__ == '__main__':
     number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
-        print("Usage: python 0-main.py <subreddit>")
-        print("Example: python 0-main.py python")
+        print("Please pass an argument for the subreddit to search.")
     else:
-        try:
-            result = number_of_subscribers(sys.argv[1])
-            if result > 0:
-                print("OK")
-            else:
-                print("Not a subreddit")
-        except Exception as e:
-            print("Error: {}".format(e))
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
