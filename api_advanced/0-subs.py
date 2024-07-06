c26fefda92fc9ef856cff553e6962422c4d0c42d
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-06 12:33:46 -0700

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 """
-A function that queries the Reddit API and returns the number of subscribers (all subscribers)
+A function that queries the Reddit API and returns the number of subscribers 
+(all subscribers)
 """
-import json  # Importing in alphabetical order
+
+import json
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of subscribers for a given 
+    subreddit.
+    
     Args:
     subreddit (str): The subreddit to query.
+    
     Returns:
     int: The number of subscribers if the subreddit is valid, 0 otherwise.
     """
     subreddit_URL = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
     headers = {"user-agent": "user"}
     response = requests.get(subreddit_URL, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         subreddit_info = response.json()
-        subscribers = subreddit_info.get("data", {}).get("subscribers")
-        return subscribers if subscribers else 0
+        subscribers = subreddit_info.get("data", {}).get("subscribers", 0)
+        return subscribers
     else:
         return 0
