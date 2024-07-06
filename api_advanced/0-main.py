934506a5e1f0c1a4a115eaecfbe5f40e1f0c2349
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-06 13:39:15 -0700

    Update 0-main.py

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3
-"""
-0-main
-"""
-import sys
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python 0-main.py <subreddit>")
+        sys.exit(1)
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+    subreddit = sys.argv[1]
+    try:
+        num_subscribers = number_of_subscribers(subreddit)
+        print("OK")
+    except Exception as e:
+        print("Not found")
+
+if __name__ == "__main__":
+    main()
