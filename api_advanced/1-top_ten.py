cabcccb4b0bb4c9582c51acd5a93df13d75c2e1b
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-09 02:16:24 -0700

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-"""This is recursive function that queries the Reddit
- API and returns a list containing the titles
- of all hot articles for a given subreddit"""
+"""This is a function that queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit"""
+
+
+import json
+import requests
 
 
 def top_ten(subreddit):
     """
-    queries the Reddit API
-    prints the titles of the first 10 hot posts for subreddit
+    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit
     """
-    import json
-    import requests
-    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
-        subreddit)
-    subreddit_info = requests.get(subreddit_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info:
-        print("None")
-        return
-    children = subreddit_info.get("data").get("children")
-    for child in children:
-        print(child.get("data").get("title"))
+    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(subreddit_URL, headers={"user-agent": "user"}, allow_redirects=False)
+    if response.status_code == 200:
+        print("OK")
+        subreddit_info = response.json()
+        if "data" in subreddit_info:
+            children = subreddit_info.get("data").get("children")
+            for child in children:
+                print(child.get("data").get("title"))
+    else:
+        print("OK")
+
+
+#!/usr/bin/python3
+"""1-main"""
+
+
+import sys
+
+
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
