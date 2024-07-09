4ce0e699a237d0fdb69ea8d66dd9a0cf27f18ec1
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-09 02:02:52 -0700

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,12 +13,17 @@ def top_ten(subreddit):
     import requests
     subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
         subreddit)
-    subreddit_info = requests.get(subreddit_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info:
+    response = requests.get(subreddit_URL,
+                            headers={"user-agent": "user"},
+                            allow_redirects=False)
+    if response.status_code == 200:
+        subreddit_info = response.json()
+        if "data" not in subreddit_info:
+            print("None")
+            return
+        children = subreddit_info.get("data").get("children")
+        for child in children:
+            print(child.get("data").get("title"))
+    else:
         print("None")
-        return
-    children = subreddit_info.get("data").get("children")
-    for child in children:
-        print(child.get("data").get("title"))
+``` and 
