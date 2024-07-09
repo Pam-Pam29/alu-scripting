fe944631ce82467481c1d45129a46402535ac592
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-09 02:46:29 -0700

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """
-This is a function that queries the Reddit API and prints the titles of the
+This is a function that queries the Reddit API and prints the titles of the 
 first 10 hot posts for a given subreddit
 """
 
 import json
 import requests
 
-
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the
+    Queries the Reddit API and prints the titles of the 
     first 10 hot posts for a given subreddit
     """
-subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
- response = requests.get(subreddit_URL, headers={"user-agent": "user"}, 
-            allow_redirects=False)
+    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(subreddit_URL, headers={"user-agent": "user"}, allow_redirects=False)
     if response.status_code == 200:
         print("OK", end='')
         subreddit_info = response.json()
@@ -24,4 +22,4 @@ subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit
             for child in children:
                 print(child.get("data").get("title"))
     else:
-        print("OK", end='')
+        print("None", end='')
