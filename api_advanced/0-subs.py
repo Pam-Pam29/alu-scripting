3a9b6fe2ecf28874e5af97734fb575297887530a
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-09 02:40:22 -0700

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
- returns the number of subscribers (all subscribers)"""
+"""
+This is a function that queries the Reddit API and prints the titles of the
+first 10 hot posts for a given subreddit
+"""
+
+import json
+import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """
-    queries the Reddit API
-    returns the number of subscribers for a given reddit
+    Queries the Reddit API and prints the titles of the
+    first 10 hot posts for a given subreddit
     """
-    import json
-    import requests
-    subreddit_URL = 'https://www.reddit.com/r/{}/about/.json'.format(
-        subreddit)
-    response = requests.get(subreddit_URL,
-                            headers={"user-agent": "user"},
-                            allow_redirects=False)
+    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(subreddit_URL, headers={"user-agent": "user"}, allow_redirects=False)
     if response.status_code == 200:
+        print("OK", end='')
         subreddit_info = response.json()
-        if "data" not in subreddit_info:
-            return 0
-        subscribers = subreddit_info.get("data").get("subscribers")
-        return subscribers
+        if "data" in subreddit_info:
+            children = subreddit_info.get("data").get("children")
+            for child in children:
+                print(child.get("data").get("title"))
     else:
-        return 0
+        print("Not a valid subreddit", end='')
