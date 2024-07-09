e562633e0aba5c250558a7236b021e71d6c710e8
Victoria Fakunle <v.fakunle@alustudent.com>
2024-07-09 02:07:08 -0700

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-"""This is a function that queries the Reddit API and
- prints the titles of the first 10 hot posts listed for a given subreddit"""
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
 
+Imports:
+    json: for handling JSON data
+    requests: for making HTTP requests to the Reddit API
+
+Functions:
+    top_ten(subreddit): queries the Reddit API and prints the titles of the first 10 hot posts for the given subreddit
+"""
+
+import json
+import requests
 
 def top_ten(subreddit):
     """
-    queries the Reddit API
-    prints the titles of the first 10 hot posts for subreddit
+    Queries the Reddit API and prints the titles of the first 10 hot posts for the given subreddit.
+
+    Args:
+        subreddit (str): the subreddit to query
+
+    Returns:
+        None
     """
-    import json
-    import requests
-    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
-        subreddit)
-    response = requests.get(subreddit_URL,
-                            headers={"user-agent": "user"},
-                            allow_redirects=False)
+    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(subreddit_URL, headers={"user-agent": "user"}, allow_redirects=False)
     if response.status_code == 200:
         subreddit_info = response.json()
         if "data" not in subreddit_info:
@@ -24,4 +34,4 @@ def top_ten(subreddit):
         for child in children:
             print(child.get("data").get("title"))
     else:
-        print("None") 
+        print("None")
